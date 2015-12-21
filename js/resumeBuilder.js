88e513cca523b5cd5700603833f6f2c1732fc622
KG001 <peifangrui@163.com>
2015-12-21 15:41:56 +0800

    define object bio

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,4 +3,30 @@ This is empty on purpose! Your code to build the resume will go here.
  */
 
 // $("#main").append("Fangrui Pei");
- $("#header").append("Fangrui Pei Project");
+// $("#header").append("Fangrui Pei Project");
+
+
+var bio = {
+	"name" : "PFR",
+	"role" : "Radar Engineer",
+	"contacts" : {
+		"mobil" : 15247184586,
+		"email" : "peifangrui@163.com",
+		"github" : "pfr223",
+		"location" : "Hohhot",
+		"twitter" : "@pfr223"
+	},
+	"welcomeMessage" : "lorem ipsum dolor sit amet etc etc etc.",
+	"skills" : [ "awesomeness", "developing things", "cryogenic sleep", "saving the universe"
+	],
+	"bioPic": "images/fry.jpg"
+}
+
+bio["city"] = "Mountain View";
+// bio.city = "Mountain View";
+bio["love"] = "GaoYao";
+// bio.love = "GaoYao";
+
+//$("#main").append(bio.city);
+
+var 
